f3f6f6217489a56cbac8d29287ddc87cbb7eada9
Porter <portersmith@ymail.com>
2024-04-30 12:10:28 -0600

    wip basic ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), tailwind()]
+});
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const EDIT_AUTH = "https://github.com/Porter-smith/does-it-scale/blob/main/services/auth/pricing.ts"
+export const EDIT_EMAIL = "https://github.com/Porter-smith/does-it-scale/blob/main/services/email/pricing.ts"
